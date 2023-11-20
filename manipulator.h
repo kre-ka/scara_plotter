25fd0fcdd056612d2c0040b254161a9c77a2a72e
Kacper Krej <kacper.krej@gmail.com>
2023-11-20 14:51:58 +0100

    added p(t) trajectory computation

--- manipulator.h
+++ manipulator.h
@@ -3,6 +3,11 @@
 
 #include <stdbool.h>
 
+// movement speed target [mm/s]
+#define V_TARGET 100.0
+// maximum acceleration (and decceleration) [mm/s^2]
+#define ACC_MAX 1000.0
+
 typedef struct {
 	float l_0;
 	float l_1;
@@ -12,5 +17,34 @@ typedef struct {
 
 bool is_in_range_angle(Manipulator *manipulator, float *theta);
 int inverse_kinematics(Manipulator *manipulator , float *in, float *out, int mode);
+
+/*
+Calculates movement phase change moments for a single curve, that is when to:
+- stop accelerating
+- start deccelerating
+- end movement
+
+Returns false and corrects v_n value if didn't manage to reach v_n (probably will never happen though).
+
+params:
+- t_phases_out - movement phase change moments output in seconds [s]
+- p_n - path lenght in millimeters [mm]
+- v_0 - initial speed [mm/s]
+- v_n - final speed [mm/s]
+- v_n_corrected - pointer to correct v_n if necessary
+*/
+bool calc_movement_time(float t_phases_out[3], float p, float v_0, float v_n, float *v_n_corrected);
+
+/*
+Calculates p(t) (position in time) trajectory in curve space based on initial speed and movement phase changes form calc_movement_time
+
+params:
+- p_tab_out - pointer to array of positions along curve in subsequent time moments in millimeters [mm]
+- t_phases - movement phase change moments from calc_movement_time in seconds [s]
+- t_res - time resolution [s]
+- p - final position (to avoid going too far) [mm]
+- v_0 - initial speed [mm/s]
+*/
+void calc_p_trajectory(float *p_tab_out, float t_phases[3], float t_res, float p, float v_0);
 
 #endif /* INC_MANIPULATOR_H_ */
